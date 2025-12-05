5d4f3c670a51ec1ae7f86c8465936829120b2b2c
Trupti Chandwani <210303105014@paruluniversity.ac.in>
2025-12-05 17:03:25 +0530

    Implemented movie details page

--- services/api.ts
+++ services/api.ts
@@ -26,16 +26,19 @@ export const fetchMovies = async ({query}: {query: string}) => {
 
 };
 
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzcxYTc2MDZmY2VkYzYyNzI4YWVjMjljOTk0OWM3ZiIsIm5iZiI6MTc1MTg2NTM0Ni40ODQsInN1YiI6IjY4NmI1ODAyN2I3NjJhZWY3MjlhM2YzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iVqGGpQj5Jqjy3zfu95_fGqupmEtuCsYUCjnZW-3iJM'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+export const fetchMovieDetails = async( movieId: string): Promise<MovieDetails> => {
+    try{
+        const response = await fetch(`${TMBD_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMBD_CONFIG.API_KEY}`,{
+            method: 'GET',
+            headers: TMBD_CONFIG.headers,
+        });
+        if(!response.ok){
+            throw new Error(`Failed to fetch movie details: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
